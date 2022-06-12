d5eb26672015290514ad2ce756646dd1c407f883
Robert Baxter Jr <rsb2194@columbia.edu>
2022-06-12 11:26:48 -0400

    Update mlbscraper.py

--- mlbscraper.py
+++ mlbscraper.py
@@ -4,9 +4,10 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 #Pull the website's source code
-url = http://www.espn.com/mlb/history/leaders/_/breakdown/season/year/2018/start/1
-page = requests.get('url')
+url = 'http://www.espn.com/mlb/history/leaders/_/breakdown/season/year/2018/'
+page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
-
-
-
+soup.find('tr', attrs ={'class' : 'oddrow player-10-3309'})
+row = soup.find('tr', attrs = {'class' : 'oddrow player-10-33039'})
+for data in  row.find_all('td'):
+    print(data.get_text())
